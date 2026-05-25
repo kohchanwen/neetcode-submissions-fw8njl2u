4d01702498ae239be93a517deefa17473d1b4f4b
ChubsWormy <34748390+kohchanwen@users.noreply.github.com>
2026-05-25 23:37:34 +0800

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> anagramMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (anagramMap.containsKey(c)) {
+                int count = anagramMap.get(c);
+                anagramMap.replace(c, ++count);
+            } else {
+                anagramMap.put(c, 1);
+            }
+        }
+
+        for (int y = 0; y < t.length(); y++) {
+            char c = t.charAt(y);
+            if (!anagramMap.containsKey(c)) {
+                return false;
+            } else {
+                int count = anagramMap.get(c);
+                if (count > 1) {
+                    anagramMap.replace(c, --count);
+                } else {
+                    anagramMap.remove(c);
+                }
+            }
+        }
+        if (anagramMap.size() != 0) {
+            return false;
+        }
+        return true;
+    }
+}
